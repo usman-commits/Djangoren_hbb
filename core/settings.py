c73ca6666dec60d732970daf8973279dc8e65340
USER <danazumiusman2@gmail.com>
2024-03-18 15:41:41 -0700

    Database config0

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0of)871%9=59ew0d+cho3@bagvek4*eak%j-+al#!#vv#-!y3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-n0of)871%9=59ew0d+cho3@bagvek4*eak%j-+al#!#vv#-!y3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
